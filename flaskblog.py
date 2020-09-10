6ea0b691d449ce955da7e2289baa1a4b7e1b0168
lopsangc2 <lopsangc2@gmail.com>
2020-09-10 21:32:03 +0545

    redirecting to login page and flash message feature added

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -37,7 +37,11 @@ def about():
 @app.route("/register")
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
+
 
 
 @app.route("/login")
