39076fc0dbabfcb2e1c4159049caf450ec3cc243
lopsangc2 <lopsangc2@gmail.com>
2020-09-11 23:01:16 +0545

    got some database update fpr the project

--- flaskblog.py
+++ flaskblog.py
@@ -1,36 +1,35 @@
 from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
-from flask_sqlalchemy import SQLALchemy
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLALchemy(app)
+db = SQLAlchemy(app)
 
 
 class User(db.Model):
-    id = db.column(db.integer, primary_key=True)
-    username = db.column(db.string(20), unique=True, nullable=False)
-    email = db.column(db.string(120), unique=True, nullable=False)
-    image_file = db.column(db.string(20),  nullable=False, default='default.jpg')
-    password = db.column(db.string(20),  nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return f"user('{self.username}', '{self.email}', '{self.image_file}')"
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
-class post(db.Model):
-    id = db.column(db.integer, primary_key=True)
-    title = db.column(db.string(20), nullable=False)
-    date_posted = db.column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.column(db.string(20), nullable=False)
-    user_id = db.column(db.integer, db.foreignkey('user.id'), nullable=False)
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return f"user('{self.title}', '{self.date_posted}')"
+        return f"Post('{self.title}', '{self.date_posted}')"
         
 
 
