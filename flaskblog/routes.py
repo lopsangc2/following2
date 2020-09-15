90eed4f6dd894d34f6f5b113fdace6f686a86c04
lopsangc2 <lopsangc2@gmail.com>
2020-09-15 14:39:51 +0545

    adede

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -69,10 +69,6 @@ def logout():
 
 @app.route("/account")
 @login_required
-def account():    
-<<<<<<< HEAD
+def account():     
     image_file = url_for('static', filename='profile_pictures/' + current_user.image_file)
     return render_template('account.html', title='account', image_file=image_file)
-=======
-    return render_template('account.html', title='account')
->>>>>>> c57438e... final of corey scafer user authentication
